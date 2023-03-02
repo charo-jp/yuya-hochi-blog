2add68adbd27aa3f47a0604b263c734f46feb2c5
Yuya Hochi <yh278@kent.ac.uk>
2023-03-02 00:31:11 +0000

    fix a bug in contact.js, made contact list reappear, separate social media data and contact.js

---
+++ src/assets/data/socialMediaData.js
@@ -0,0 +1,17 @@
+import linkedin from "../images/icons8-linkedin-circled.svg"
+import email from "../images/icons8-mail.svg"
+
+const socialMediaData = [
+    {
+        image: linkedin,
+        alt: "Linkedin",
+        href: "https://www.linkedin.com/in/yuya-hochi-my-profile/"
+    },
+    {
+        image: email,
+        alt: "Gmail",
+        href: "yuyahochi4blog@gmail.com"
+    }
+];
+
+export default socialMediaData;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,10 @@ import React from "react";
 import { Card, CardMedia, CardActionArea, CardHeader } from "@mui/material";
 import Layout from "../components/Layout/Layout";
 
-// add icons here
-import linkedin from "../assets/images/icons8-linkedin-circled.svg";
-import email from "../assets/images/icons8-mail.svg";
 import SEO from "../components/SEO";
 import "./contact.scss";
 
-const socialMediaData = [
-  {
-    image: linkedin,
-    alt: "Linkedin",
-    href: "https://www.linkedin.com/in/yuya-hochi-my-profile/",
-  },
-  {
-    image: email,
-    alt: "Gmail",
-    href: "yuyahochi4blog@gmail.com",
-  },
-];
+import socialMediaData from "../assets/data/socialMediaData";
 
 const contact = () => {
   return (
@@ -32,30 +18,16 @@ const contact = () => {
         <div className="contact">
           <h1>Contact</h1>
           <div className="contact-links">
-            <div className="social-media">
-              {socialMediaData.map((item, index) => {
-                return (
-                  <Card key={index}>
-                    <CardActionArea
-                      href={item.alt === "Gmail" ? "" : item.href}
-                      target="_blank"
-                    >
-                      <CardHeader
-                        title={item.alt}
-                        subheader={
-                          item.alt === "Gmail" ? "yuyahochi4blog@gmail.com" : ""
-                        }
-                      />
-                      <CardMedia
-                        component="img"
-                        src={item.image}
-                        alt={item.alt}
-                      />
-                    </CardActionArea>
-                  </Card>
-                );
-              })}
-            </div>
+            {socialMediaData.map((item, index) => {
+              return (
+                <Card key={index}>
+                  <CardActionArea href={item.alt === "Gmail" ? "" : item.href} target="_blank">
+                    <CardHeader title={item.alt} subheader={item.alt === "Gmail" ? "yuyahochi4blog@gmail.com" : ""} />
+                    <CardMedia component="img" src={item.image} alt={item.alt} />
+                  </CardActionArea>
+                </Card>
+              );
+            })}
           </div>
         </div>
       </div>
